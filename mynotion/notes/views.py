d89c20c468813d92474c5b5f6487a3cb975975bc
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2022-09-01 13:31:21 +0200

    Uses Django dummy template to show the notes.

--- mynotion/notes/views.py
+++ mynotion/notes/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 def notes(request, note=''):
-    status_code = 200
     if note == '':
-        note = 'New-note'
-        status_code = 301
-    value = HttpResponse('This is the main page for notes' + str(note))
-    value.headers['Location'] = note
-    value.status_code = status_code
+        return redirect(template_name='note.html', note='New-note')
+    else:
+        return render(request, template_name='note.html')
     
-    return value
 # Create your views here.
