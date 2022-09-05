a55444fcf05e2da8e2a9f4a55134f3538843060d
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2022-09-05 12:45:09 +0200

    Use UUID as a note ID.

--- mynotion/notes/models.py
+++ mynotion/notes/models.py
@@ -4,9 +4,11 @@ import uuid
 
 # Create your models here.
 class Note(models.Model):
-    id = models.UUIDField(uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     slug = models.SlugField(max_length=100)
     title = models.CharField(max_length=100)
     content = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return str(self.slug) + "-" + str(self.id)
