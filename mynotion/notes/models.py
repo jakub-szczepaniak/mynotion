4085541a8c8821a948c97fe0fdcca4512c4859a0
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2022-09-05 12:23:53 +0200

    Adds model for single note.

--- mynotion/notes/models.py
+++ mynotion/notes/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
+import uuid
 
 # Create your models here.
+class Note(models.Model):
+    id = models.UUIDField(uuid.uuid4, unique=True, primary_key=True, editable=False)
+    slug = models.SlugField(max_length=100)
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
