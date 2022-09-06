c7c375fba61ae78014490f6c1683bf4de07b932e
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2022-09-06 10:15:37 +0200

    Adds relationship between Note and text block.

--- mynotion/notes/admin.py
+++ mynotion/notes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Note
+from .models import Note, Block
 
 admin.site.register(Note)
+admin.site.register(Block)
--- mynotion/notes/models.py
+++ mynotion/notes/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 import uuid
 
@@ -7,8 +6,17 @@ class Note(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     slug = models.SlugField(max_length=100)
     title = models.CharField(max_length=100)
-    content = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return str(self.slug) + "-" + str(self.id)
+
+
+class Block(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    content = models.TextField(null=True, blank=True)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE, null=True, blank=True)
+    tag = models.CharField(max_length=10,null=False, default="p", blank=False)
+
+    def __str__(self):
+        return str(self.tag) + " in note: " + self.note.title
